a966af5a385ae1ec5e8c7300e9d7223596e9d3a3
Karlotcha <KHOA@HOUSETRIP.COM>
2013-09-15 21:27:41 +0100

    v2

--- PT_storylifecycle.js
+++ PT_storylifecycle.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         PT - full story life cycle
-// @version      0.1
+// @version      0.2
 // @description  track stories to PR and to deploy
 // @match        https://www.pivotaltracker.com/s/projects/*
 // @author       Karlotcha Hoa
